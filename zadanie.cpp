42665a31bb364ed393b35c42c3821c6821748bce
James Sevil <jsclassified24@gmail.com>
2024-03-15 01:51:51 +0700

    Добавил проверку сторон

--- zadanie.cpp
+++ zadanie.cpp
@@ -5,12 +5,15 @@ using namespace std;
 int main(){
     setlocale(LC_ALL, "Rus");
 
-    cout << "Введите 2 стороны прямоугольника: " << endl;
     int a, b;
-    cin >> a >> b;
+    do {
+        cout << "Введите 2 стороны прямоугольника(стороны не должны быть равные): ";
+        cin >> a >> b;
+    }
+    while (a==b);
     cout << "Периметр прямоугольника: " << 2*a+2*b << endl;
     cout << "Площадь прямоугольника: " << a*b << endl;
-    cout << "Длина диагоняли: " << sqrt(a*a+b*b) << endl;
+    cout << "Длина диагонали: " << sqrt(a*a+b*b) << endl;
     
     return 0;
 }
